4e3a6b31a32bc415b572658bb3bbaaae0dd540eb
Matt McKnight <mmcknight@egov.com>
2015-08-08 15:02:57 -0400

    JavaScript implementation

---
+++ MetricClock.Web/metric-clock.js
@@ -0,0 +1,35 @@
+﻿
+function MetricClock() {
+
+
+    this.getTime = function getTime() {
+        var secRatio = (25.0 / 18.0);
+
+        var now = new Date();
+        var midnight = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 0, 0, 0, 0);
+        var startOfDay = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 6, 0, 0, 0);
+        var metricSecsSinceStart, totalSecondsToday;
+
+        totalSecondsToday = (now - midnight) / 1000;
+        if (now >= midnight && now < startOfDay) {
+            //this means we are between 10 and 1 (10,11,12,1)
+            metricSecsSinceStart = 90000 + (secRatio * totalSecondsToday);
+        }
+        else {
+            var standardSecs = totalSecondsToday - 21600;// start.TimeOfDay.TotalSeconds;
+            metricSecsSinceStart = standardSecs * secRatio;
+        }
+
+        var currentMetricHours = Math.floor(metricSecsSinceStart / 10000);
+
+        var currentMetricMinutes = Math.floor((metricSecsSinceStart - (currentMetricHours * 10000)) / 100);
+        var currentMetricSeconds = Math.floor(metricSecsSinceStart - (currentMetricHours * 10000) - (currentMetricMinutes * 100));
+        if (currentMetricHours == 12)
+            currentMetricHours = 0;
+        var stringTime = (1 + currentMetricHours).toString() + ":" + currentMetricMinutes.toString() + ":" + currentMetricSeconds.toString();
+
+        return stringTime;
+    }
+}
+
+var metricClock = new MetricClock();
